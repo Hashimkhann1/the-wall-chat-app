cd6a2c7db930414408663d5b61b2925c195b93b3
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-23 22:11:50 +0500

    for dark theme

---
+++ lib/theme/dark_them.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    background: Colors.black,
+  ),
+);
